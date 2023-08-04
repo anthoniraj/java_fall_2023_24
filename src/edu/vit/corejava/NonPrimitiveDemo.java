e05ab5ccedccc5a584c7feb4bced1c505fb31fc9
anthoniraj <anthoniraja@gmail.com>
2023-08-04 08:41:40 +0530

    added variable concept

---
+++ src/edu/vit/corejava/NonPrimitiveDemo.java
@@ -0,0 +1,14 @@
+/*
+ * Demo Program
+ * @author Anthoniraj Amalanathan
+ * @since 03-08-2023
+ */
+
+import java.util.Scanner;
+
+public class NonPrimitiveDemo {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+        }
+    }
+} 
